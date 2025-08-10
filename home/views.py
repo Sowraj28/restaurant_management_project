219734f99420bc6a0353f54ae1b3005e665f5148
Sowraj <sowrajsethu2004@gmail.com>
2025-08-10 23:03:17 +0530

    28347

--- home/views.py
+++ home/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
-from .
+from .models import Restaurant 
 
 # Create your views here.
+def homepage(request):
+    restaurant=Restaurant.objects.first()
+    context={
+        'restaurant_name':restaurant.name if restaurant else 'Restaurant Name Not Set',
+        'restaurant_tagline':restaurant.tagline if restaurant else '',
+    }
+    return render(request,'home/home.html',context)
