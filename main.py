1ee6cdb99c4a76e14ffea2eb7fe50689fb77c84c
Sowraj <sowrajsethu2004@gmail.com>
2025-08-10 23:28:22 +0530

    28347

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from django.core.management import execute_from_command_line
+
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE','restaurant_management.settings')
+    execute_from_command_line(['manage.py','runserver'])
+
+if __name__=="main":
+    main()
