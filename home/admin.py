acd25b0d9d678596c3f3299620a851f88c74a266
Sowraj <sowrajsethu2004@gmail.com>
2025-08-10 22:56:20 +0530

    28347

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Restaurant
 
 # Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display=('name','tagline','created_at','updated_at')
+    search_fields=('name','tagline')
+    
