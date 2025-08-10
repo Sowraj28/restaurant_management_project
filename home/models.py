3b5c0e9ec052865179f9d26efde6f1c13cbf9b4c
Sowraj <sowrajsethu2004@gmail.com>
2025-08-10 22:52:21 +0530

    28347

--- home/models.py
+++ home/models.py
@@ -3,4 +3,14 @@ from django.db import models
 # Create your models here.
 class Restaurant(models.Model):
     name=models.CharField(max_length=100,unique=True)
-    tagline=models.CharField(max_length=255,blank=True)
+    tagline=models.CharField(max_length=255,blank=True,null=True)
+    Create_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name="Restaurant"
+        verbose_name_pural="Restaurants"
+        ordering=['name']
+
+        def __str__(self):
+            return self.name
