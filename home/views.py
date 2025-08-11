782ebfd6b4e4bf30f5833752d5ffadb6927a20fa
Sowraj <sowrajsethu2004@gmail.com>
2025-08-11 13:04:37 +0530

    28348

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Restaurant 
+from .models import About
 
 # Create your views here.
 def homepage(request):
@@ -9,3 +10,12 @@ def homepage(request):
         'restaurant_tagline':restaurant.tagline if restaurant else '',
     }
     return render(request,'home/home.html',context)
+
+def about(request):
+    about_info=About.objects.firts()
+    context={
+        'title':about_info.title if about_info else "About Us",
+        'description':about_info.description if about_info else "Description not set",
+        'image_url':about_info.image_url if about_info and about_info.image else None,
+    }
+    return render(request,'home/about.html',context)
