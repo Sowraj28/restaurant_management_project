b0972ad86e8121add1bcfaceecc01f943c7fec1c
Sowraj <sowrajsethu2004@gmail.com>
2025-08-12 12:02:13 +0530

    30436

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import homepage,about,contact
+from .views import homepage,about,contact,menu_view
 
 urlpatterns = [
     path('',homepage,name='home'),
     path('about/',about,name='about'),
     path('contact/',contact,name='contact'),
+    path('menu/',menu_view,name='menu'),
 ]
