1f8e7c446f54409a68d82f361b2c9343c5ab0084
Sowraj <sowrajsethu2004@gmail.com>
2025-08-14 10:30:57 +0530

    32293

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Restaurant(models.Model):
     name=models.CharField(max_length=100,unique=True)
     tagline=models.CharField(max_length=255,blank=True,null=True)
+    logo=models.ImageField(upload_to='restaurant_logos/',blank=True,null=True)
     Create_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
