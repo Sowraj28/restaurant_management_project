3abbe9286a36ac6aaba2a74e6b1540585e23bfa5
Sowraj <sowrajsethu2004@gmail.com>
2025-08-17 20:04:02 +0530

    34260

--- home/forms.py
+++ home/forms.py
@@ -1,2 +1,14 @@
 from django import forms
 from .models import Feedback
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model=Feedback
+        fields=['comment']
+        widgets={
+            'comment':forms.Textarea(attrs={
+                'placeholder':'leave Your Feedback Here...',
+                'rows':4,
+                'cols':40
+            })
+        }
