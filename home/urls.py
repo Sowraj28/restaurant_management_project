f7ab0b2e90ed03274f597faf7b68cdb705a2d502
Sowraj <sowrajsethu2004@gmail.com>
2025-08-17 20:09:56 +0530

    34260

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import homepage,about,contact,menu_view
+from .views import homepage,about,contact,menu_view,feedback_view
 
 urlpatterns = [
     path('',homepage,name='home'),
     path('about/',about,name='about'),
     path('contact/',contact,name='contact'),
     path('menu/',menu_view,name='menu'),
+    path('feedback/',feedback_view,name='feedback')
 ]
