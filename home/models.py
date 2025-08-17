00b844b668170a2101d53ccbd9f680d6ef9c9c94
Sowraj <sowrajsethu2004@gmail.com>
2025-08-17 19:59:12 +0530

    34260

--- home/models.py
+++ home/models.py
@@ -46,3 +46,9 @@ class RestaurantInfo(models.Model):
     def __str__(self):
         return self.name
 
+class Feedback(models.Model):
+    comment=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback {self.id} - {self.created_at.strftime('%Y-%m-%d %H:%M')}"
