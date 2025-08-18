27ba847ffaba965ca5d46206ffbaba8a3df37a64
Sowraj <sowrajsethu2004@gmail.com>
2025-08-18 16:04:46 +0530

    34939

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Restaurant(models.Model):
@@ -55,5 +56,10 @@ class Feedback(models.Model):
 
 
 
-class Userprofile(models.Model):
-    user=models.OneToOneField(User,on_delete=CAS)
+class UserProfile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    full_name=models.CharField(max_length=100)
+    phone_number=models.CharField(max_length=15 ,blank=True,null=True)
+
+    def __str__(self):
+        return self.full_name or self.user.username
