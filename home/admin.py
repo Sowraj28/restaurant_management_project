5842561bbaaf93e1691ad3427b6e7780ca251a7f
Sowraj <sowrajsethu2004@gmail.com>
2025-08-18 20:02:59 +0530

    34942

--- home/admin.py
+++ home/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Restaurant
 from .models import About
 from .models import RestaurantInfo
-
+from .models import Menu,Order
 # Register your models here.
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
@@ -14,3 +14,16 @@ class AboutAdmin(admin.ModelAdmin):
     list_display=('title','updated_at')
 
 admin.site.register(RestaurantInfo)
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display=('name','price','available')
+    search_fields=('name',)
+    list_filter=('available',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=('customer_name','menu_items','quantity','created_at')
+    search_fields=('customer_name',)
+    list_filter=('created_at',)
