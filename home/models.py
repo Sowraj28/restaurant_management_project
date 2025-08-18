66eaf6edcb25a56006f855b0ef190af14e6e6078
Sowraj <sowrajsethu2004@gmail.com>
2025-08-18 15:33:58 +0530

    34939

--- home/models.py
+++ home/models.py
@@ -52,3 +52,8 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback {self.id} - {self.created_at.strftime('%Y-%m-%d %H:%M')}"
+
+
+
+class Userprofile(models.Model):
+    name=models.CharField()
