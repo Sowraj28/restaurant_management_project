10909bafb97face029713f12f9ebcb3e2aa3a365
Sowraj <sowrajsethu2004@gmail.com>
2025-08-19 11:47:12 +0530

    34943

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import homepage,about,contact,menu_view,feedback_view
+from .import views
+app_name='restaurant'
 
 urlpatterns = [
-    path('',homepage,name='home'),
-    path('about/',about,name='about'),
-    path('contact/',contact,name='contact'),
-    path('menu/',menu_view,name='menu'),
-    path('feedback/',feedback_view,name='feedback')
+    path('',views.homepage,name='home'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('menu/',views.menu_view,name='menu'),
+    path('feedback/',views.feedback_view,name='feedback')
 ]
