35ca17d1e15af7c09f949dbc98fb13534d3b1df0
Sowraj <sowrajsethu2004@gmail.com>
2025-08-20 22:43:46 +0530

    35789

--- home/models.py
+++ home/models.py
@@ -70,10 +70,10 @@ class Menu(models.Model):
     price=models.DecimalField(max_digits=6,decimal_places=2)
     description=models.TextField(blank=True)
     available=models.BooleanField(default=True)
-
+    created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}-Rs.{self.price}"
 
 class Order(models.Model):
     STATUS_CHOICE=[
