c53098224ef933d83e5859cfbf8ac23ab520730f
Sowraj <sowrajsethu2004@gmail.com>
2025-08-20 23:51:13 +0530

    35787

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Feedback
+from .models import contact
 
 
 class FeedbackForm(forms.ModelForm):
@@ -12,4 +13,13 @@ class FeedbackForm(forms.ModelForm):
                 'rows':4,
                 'cols':40
             })
+        }
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model=contact
+        fields=['name','email']
+        widgets={
+            'name':forms.TextInput(attrs={'placeholder':'Your Name','class':'form-input'}),
+            'email':forms.EmailInput(attrs={'placeholder':'Your Email','class':'form=input'}),
         }
