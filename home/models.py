ee647133684f2517f24b814b970b4e5b33e60b5d
Sowraj <sowrajsethu2004@gmail.com>
2025-08-25 14:47:41 +0530

    38309

--- home/models.py
+++ home/models.py
@@ -105,3 +105,14 @@ class OrderItem(models.Model):
 
     def get_subtotal(self):
         return self.menu_item.price*self.quantity
+
+
+class RestaurantLocation(models.Model):
+    address=models.CharField(max_length=255)
+    city=models.CharField(max_length=150)
+    state=models.CharField(max_length=100)
+    zip_code=models.CharField(max_lenght=50)
+
+    def __str__(self):
+        return f"{self.address},{self.city},{self.state},{self.zip_code}"
+    
