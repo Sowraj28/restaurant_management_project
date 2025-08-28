cb77873328394c79cbfbfe0c2757b931030f5fdd
Sowraj <sowrajsethu2004@gmail.com>
2025-08-28 09:44:11 +0530

    39765

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.models import user
 
 from .models import Item
 from .serializers import ItemSerializer
@@ -24,3 +25,11 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CartItem(models.Model):
+    user=models.foreginkey(User,on_delete=models.CASADE)
+    item_name=models.CharField(max_lenght=200)
+    quantity=models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        returnf"{self.item_name} {self.quantity}"
